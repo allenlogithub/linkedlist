a6beaa4b70057a406a90125ca95e9d8608745cfc
root <allenlo0130@gmail.com>
2021-12-17 07:54:39 +0000

    [add] add test case:TestDLLPushAt

--- linkedlist/doublylinkedlist_test.go
+++ linkedlist/doublylinkedlist_test.go
@@ -1,4 +1,5 @@
 /*
+	https://stackoverflow.com/questions/16935965/how-to-run-test-cases-in-a-specified-file
 	go clean -testcache && go test -v linkedlist
 	go clean -testcache && go test -v linkedlist/doublylinkedlist_test.go linkedlist/doublylinkedlist.go
 */
@@ -8,6 +9,8 @@ package linkedlist
 import (
 	"testing"
 	"reflect"
+	// "fmt"
+	// "errors"
 )
 
 func (l *DoublyLinkedList) traverse() []interface{} {
@@ -50,5 +53,72 @@ func TestDLLPush(t *testing.T) {
 	}
 	if l.getLength() != 3 {
 		t.Errorf("DLL.Push failed")
+	}
+}
+
+func TestDLLPushAt(t *testing.T) {
+	l := DoublyLinkedList{}
+
+	// error check
+	if err := l.PushAt(1, 3); err != nil {		
+		if err.Error.Error() != "IndexError" {
+			t.Errorf("DLL.PushAt failed")
+		}
+	}
+
+	// error check
+	l.Push(1)
+	if err := l.PushAt(2, 3); err != nil {		
+		if err.Error.Error() != "IndexError" {
+			t.Errorf("DLL.PushAt failed")
+		}
+	}
+
+	// pos == 0 check
+	l.PushAt(0, 0)
+	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 1}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if !reflect.DeepEqual(l.revTraverse(), []interface{}{1, 0}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if l.getLength() != 2 {
+		t.Errorf("DLL.PushAt failed")
+	}
+
+	// pos == l.length check
+	l.PushAt(2, 2)
+	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 1, 2}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1, 0}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if l.getLength() != 3 {
+		t.Errorf("DLL.PushAt failed")
+	}
+
+	// pos > l.length/2 check
+	l.PushAt(1, 0.5)
+	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 0.5, 1, 2}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1, 0.5, 0}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if l.getLength() != 4 {
+		t.Errorf("DLL.PushAt failed")
+	}
+
+	// pos <= l.length/2 check
+	l.PushAt(3, 1.5)
+	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 0.5, 1, 1.5, 2}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1.5, 1, 0.5, 0}) {
+		t.Errorf("DLL.PushAt failed")
+	}
+	if l.getLength() != 5 {
+		t.Errorf("DLL.PushAt failed")
 	}
 }
