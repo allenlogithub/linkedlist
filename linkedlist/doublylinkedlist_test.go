0dfb43ea4e1cce31aa43458a2dd8b13e96b11536
root <allenlo0130@gmail.com>
2021-12-17 08:10:57 +0000

    [mod] refactor

--- linkedlist/doublylinkedlist_test.go
+++ linkedlist/doublylinkedlist_test.go
@@ -7,8 +7,8 @@
 package linkedlist
 
 import (
-	"testing"
 	"reflect"
+	"testing"
 	// "fmt"
 	// "errors"
 )
@@ -27,7 +27,7 @@ func (l *DoublyLinkedList) traverse() []interface{} {
 func (l *DoublyLinkedList) revTraverse() []interface{} {
 	var values []interface{}
 	n := l.Tail
-	for i := l.length - 1; i >= 0; i -- {
+	for i := l.length - 1; i >= 0; i-- {
 		values = append(values, n.Value)
 		n = n.Pre
 	}
@@ -39,28 +39,32 @@ func (l *DoublyLinkedList) getLength() int {
 	return l.length
 }
 
+func (l *DoublyLinkedList) checkDLL(t *testing.T, forward []interface{}, backward []interface{}, length int, funcName string) {
+	if !reflect.DeepEqual(l.traverse(), forward) {
+		t.Errorf("DLL.%s failed", funcName)
+	}
+	if !reflect.DeepEqual(l.revTraverse(), backward) {
+		t.Errorf("DLL.%s failed", funcName)
+	}
+	if l.getLength() != length {
+		t.Errorf("DLL.%s failed", funcName)
+	}
+}
+
 func TestDLLPush(t *testing.T) {
 	l := DoublyLinkedList{}
 
 	l.Push(1)
 	l.Push("a")
 	l.Push(1.0)
-	if !reflect.DeepEqual(l.traverse(), []interface{}{1, "a", 1.0}) {
-		t.Errorf("DLL.Push failed")
-	}
-	if !reflect.DeepEqual(l.revTraverse(), []interface{}{1.0, "a", 1}) {
-		t.Errorf("DLL.Push failed")
-	}
-	if l.getLength() != 3 {
-		t.Errorf("DLL.Push failed")
-	}
+	l.checkDLL(t, []interface{}{1, "a", 1.0}, []interface{}{1.0, "a", 1}, 3, "Push")
 }
 
 func TestDLLPushAt(t *testing.T) {
 	l := DoublyLinkedList{}
 
 	// error check
-	if err := l.PushAt(1, 3); err != nil {		
+	if err := l.PushAt(1, 3); err != nil {
 		if err.Error.Error() != "IndexError" {
 			t.Errorf("DLL.PushAt failed")
 		}
@@ -68,7 +72,7 @@ func TestDLLPushAt(t *testing.T) {
 
 	// error check
 	l.Push(1)
-	if err := l.PushAt(2, 3); err != nil {		
+	if err := l.PushAt(2, 3); err != nil {
 		if err.Error.Error() != "IndexError" {
 			t.Errorf("DLL.PushAt failed")
 		}
@@ -76,49 +80,17 @@ func TestDLLPushAt(t *testing.T) {
 
 	// pos == 0 check
 	l.PushAt(0, 0)
-	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 1}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if !reflect.DeepEqual(l.revTraverse(), []interface{}{1, 0}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if l.getLength() != 2 {
-		t.Errorf("DLL.PushAt failed")
-	}
+	l.checkDLL(t, []interface{}{0, 1}, []interface{}{1, 0}, 2, "PushAt")
 
 	// pos == l.length check
 	l.PushAt(2, 2)
-	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 1, 2}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1, 0}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if l.getLength() != 3 {
-		t.Errorf("DLL.PushAt failed")
-	}
+	l.checkDLL(t, []interface{}{0, 1, 2}, []interface{}{2, 1, 0}, 3, "PushAt")
 
 	// pos > l.length/2 check
 	l.PushAt(1, 0.5)
-	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 0.5, 1, 2}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1, 0.5, 0}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if l.getLength() != 4 {
-		t.Errorf("DLL.PushAt failed")
-	}
+	l.checkDLL(t, []interface{}{0, 0.5, 1, 2}, []interface{}{2, 1, 0.5, 0}, 4, "PushAt")
 
 	// pos <= l.length/2 check
 	l.PushAt(3, 1.5)
-	if !reflect.DeepEqual(l.traverse(), []interface{}{0, 0.5, 1, 1.5, 2}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if !reflect.DeepEqual(l.revTraverse(), []interface{}{2, 1.5, 1, 0.5, 0}) {
-		t.Errorf("DLL.PushAt failed")
-	}
-	if l.getLength() != 5 {
-		t.Errorf("DLL.PushAt failed")
-	}
+	l.checkDLL(t, []interface{}{0, 0.5, 1, 1.5, 2}, []interface{}{2, 1.5, 1, 0.5, 0}, 5, "PushAt")
 }
