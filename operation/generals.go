7ee6a9409278f488449e01fdbb4c2494772d90e1
root <allenlo0130@gmail.com>
2021-12-28 09:13:29 +0000

    [add] add operation.func:ToDLL for converting sting integer to DLL

--- operation/generals.go
+++ operation/generals.go
@@ -29,3 +29,19 @@ func (l *DLL) ToString() string {
 		ptr = ptr.Pre
 	}
 }
+
+// "123" 
+// -> DLL.head.Value = 3, DLL.head.Next.Value = 2, DLL.head.Next.Next.Value = 1
+func ToDLL(s string) (*DLL, *OperationError) {
+    l := DLL{}
+    for i := len(s) - 1; i >= 0; i-- {
+        if err := l.Push(int(s[i]-48)); err != nil {
+            return nil, &OperationError{
+                err.Error,
+                err.Message,
+            }
+        }
+    }
+
+    return &l, nil
+}
