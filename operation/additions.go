0faa307b59f4b4565dac9cc33f2c4643f7dac096
root <allenlo0130@gmail.com>
2021-12-28 10:11:44 +0000

    [mod] operation.DLLIntAdd: bug fix

--- operation/additions.go
+++ operation/additions.go
@@ -57,25 +57,55 @@ func IntAdd(a, b string) (*DLL, *OperationError) {
 // input: DLL
 // output: DLL
 func DLLIntAdd(a, b *DLL) (*DLL, *OperationError) {
- ptrA, ptrB, carry, l := a.Head, b.Head, 0, DLL{}    
- for {
-     sum := ptrA.Value.(int) + ptrB.Value.(int) + carry
-     if sum > 9 {
-         sum = sum % 10
-         carry = 1
-     } else {
-         carry = 0
-     }
-     if err := l.Push(sum); err != nil {
-         return &l, &OperationError{
-             err.Error,
-             err.Message,
-         }
-     }
-     ptrA = ptrA.Next
-     ptrB = ptrB.Next
-     if ptrA == nil {
-         return &l, nil
-     }
- }
+	if a.Length < b.Length {
+		a, b = b, a
+	}
+	ptrA, ptrB, carry, l := a.Head, b.Head, 0, DLL{}
+	
+	for i := 0; i < b.Length; i++ {
+		sum := ptrA.Value.(int) + ptrB.Value.(int) + carry
+		if sum > 9 {
+			sum = sum % 10
+			carry = 1
+		} else {
+			carry = 0
+		}
+		if err := l.Push(sum); err != nil {
+			return &l, &OperationError{
+				err.Error,
+				err.Message,
+			}
+		}
+		ptrA, ptrB = ptrA.Next, ptrB.Next		
+	}
+	if ptrA != nil {			
+		for {			
+			if ptrA == nil {
+				break
+			}
+			sum := ptrA.Value.(int) + carry
+			if sum > 9 {
+				sum = sum % 10
+				carry = 1
+			} else {
+				carry = 0
+			}			
+			if err := l.Push(sum); err != nil {
+				return &l, &OperationError{
+					err.Error,
+					err.Message,
+				}
+			}
+			ptrA = ptrA.Next
+		}			 		
+	}
+	if carry == 1 {
+		if err := l.Push(1); err != nil {
+			return &l, &OperationError{
+				err.Error,
+				err.Message,
+			}
+		}
+	}		
+	return &l, nil
 }
