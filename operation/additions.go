eee9e2b24fdd7198d8529f7a26ee79920efeff13
root <allenlo0130@gmail.com>
2021-12-31 03:21:48 +0000

    [del] remove operation.func:IntAdd

--- operation/additions.go
+++ operation/additions.go
@@ -1,59 +1,6 @@
 package operation
 
-import (
-	"errors"
-	"strings"
-)
-
-// sum of two natural int
-// input: string
-// output: DLL
-func IntAdd(a, b string) (*DLL, *OperationError) {
-	l := DLL{}
-
-	if len(a) < len(b) {
-		a, b = b, a
-	}
-
-	if (a[0] == 48 && len(a) > 1) || (b[0] == 48 && len(b) > 1) {
-		return &l, &OperationError{
-			errors.New("InputError"),
-			"First char of the input shouldn't be 0",
-		}
-	}
-
-	b = strings.Repeat("0", len(a)-len(b)) + b
-
-	carry := 0
-	for i := len(a) - 1; i >= 0; i-- {
-		a, b := int(a[i]-48), int(b[i]-48)
-		sum := a + b + carry
-		if sum > 9 {
-			sum = sum % 10
-			carry = 1
-		} else {
-			carry = 0
-		}
-		if err := l.Push(sum); err != nil {
-			return &l, &OperationError{
-				err.Error,
-				err.Message,
-			}
-		}
-	}
-	if carry == 1 {
-		if err := l.Push(1); err != nil {
-			return &l, &OperationError{
-				err.Error,
-				err.Message,
-			}
-		}
-	}
-
-	return &l, nil
-}
-
-// sum of two natural int
+// sum of two natural numbers
 // input: DLL
 // output: DLL
 func DLLIntAdd(a, b *DLL) (*DLL, *OperationError) {
@@ -107,5 +54,6 @@ func DLLIntAdd(a, b *DLL) (*DLL, *OperationError) {
 			}
 		}
 	}
+
 	return &l, nil
 }
